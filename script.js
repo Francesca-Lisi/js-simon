bffcb904bb11d4badbc19ec35b35d06070962ddd
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-26 22:08:13 +0100

    feat: add countdown

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’or
 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri 
 da indovinare sono stati individuati.*/
+
+
+// Richiamo elementi DOM
+const countdown = document.getElementById('countdown');
+let seconds = 10;
+const numRandom = document.getElementById('numeri-random');
+const numField = document.getElementById('numbers-field');
+const rules = document.getElementById('rules');
+const result =document.getElementById('result');
+const btn = document.getElementById('btn');
+const btnStart = document.getElementById('btn-start');
+
+
+//Creo il Countdown
+countdown.innerHTML = seconds--;
+
+const intervalId = setInterval(function(){
+
+  countdown.innerHTML = seconds;
+  if(seconds === 0){
+    clearInterval(intervalId);
+    countdown.innerHTML = 'Via!';
+    rules.innerHTML = 'Inserisci i numeri memorizzati';
+    numRandom.classList.add('d-none');
+    numField.classList.remove('d-none');
+    btn.classList.remove('d-none');
+  }
+  seconds--;
+
+},500)
+
+//Creo i numeri random

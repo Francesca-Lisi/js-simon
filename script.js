1024f425295c148b15a4f17b44c8701a85bcb6af
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-29 01:10:23 +0100

    feat: add result and img

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ const numRandom = document.getElementById('numeri-random');
 const rules = document.getElementById('rules');
 const result =document.getElementById('result');
 const form = document.getElementById('numbers-field')
-const imputFields = document.querySelectorAll('imput')
+const inputFields = document.querySelectorAll('input')
+const image= document.getElementById('image');
+const winner = document.getElementById('winner')
+const message = document.getElementById('message')
 let seconds = 10;
 const min = 1;
 const max = 50;
@@ -31,13 +34,14 @@ const intervalId = setInterval(function(){
   if(seconds === 0){
     clearInterval(intervalId);
     countdown.innerHTML = 'Via!';
-    rules.innerHTML = 'Inserisci i numeri memorizzati';
+    rules.innerHTML = 'Inserisci i numeri memorizzati'
     numRandom.classList.add('d-none');
     form.classList.remove('d-none');
+    image.innerHTML = `<img src="img/Fox_write_2.JPG" alt="Fox Write" class="img-fluid">`;
   }
   seconds--;
 
-},200)
+},1000)
 
 //Creo i numeri random
 
@@ -56,16 +60,15 @@ numRandom.innerHTML = randomNumbers.join(' - ')
 //creo una funzione che mi restituisca un array con i valori inseriti
 
 
-
-const confirm = e => {
+form.addEventListener ('submit', (e) => {
   e.preventDefault();
 
   const playerNumbers = [];
-  for (let i = 0; i < imputFields.length ; i++){
+  for (let i = 0; i < inputFields.length ; i++){
     const field = inputFields[i];
     const value = parseInt(field.value);
   
-    if(!isNaN(value) && value>= min && value <= max && !playerNumbers.includes(value)){
+    if(!isNaN(value) && value >= min && value <= max && !playerNumbers.includes(value)){
       playerNumbers.push(value)
     }
   } 
@@ -73,21 +76,36 @@ const confirm = e => {
   if (playerNumbers.length !== totalNumbers){
     countdown.classList.add('text-danger');
     countdown.innerHTML = 'Attenzione! Inserisci 5 numeri diversi';
+    image.innerHTML = `<img src="img/Fox_error_4.JPG" alt="Fox Error" class="img-fluid">`;
     return;
   }
-
+  console.log(playerNumbers)
+  
   const correctNumbers = [];
   for (let i = 0; i < playerNumbers.length; i++){
     const guess = playerNumbers[i];
     if(randomNumbers.includes(guess)) correctNumbers.push(guess)
   }
 
-  const guessString = correctAswers.join(' - ');
+  const guessString = correctNumbers.join(' - ');
 
   console.log(guessString)
-}
 
-form.addEventListener('submit', confirm)
+  result.classList.remove('d-none')
+  if (correctNumbers.length < 1) {
+    image.innerHTML = `<img src="img/Fox_looser_5.JPG" alt="Fox Looser" class="m-1" class="img-fluid">`;
+    winner.innerHTML = 'HAI PERSO!';
+    message.innerHTML = 'Non hai indovinato nessun numero'
+  } else if (correctNumbers.length > 4){
+    image.innerHTML = `<img src="img/Fox_perfect_6.JPG" alt="Fox Perfect" class="m-1" class="img-fluid">`;
+    winner.innerHTML = 'PERFETTO!';
+    message.innerHTML = `Hai indovinato tutti i numeri: ${guessString}`;
+  } else {
+    image.innerHTML = `<img src="img/Fox_good_3.JPG" alt="Fox Good" class="m-1" class="img-fluid">`;
+    winner.innerHTML = 'HAI VINTO!';
+    message.innerHTML = `Hai indovinato i seguenti numeri: ${guessString}`;
+  }
+})
 
 
 //bg-danger-subtle border-3 border-danger-subtle
